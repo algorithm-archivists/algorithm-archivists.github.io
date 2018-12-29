4ea4d651590879fe993c141a81482a2243870ffe
leios <jrs.schloss@gmail.com>
2018-12-30 08:47:55 +0900

    manual site update

--- contents/convolutions/code/julia/conv.jl
+++ contents/convolutions/code/julia/conv.jl
@@ -1,11 +1,13 @@
-function conv(signal1::Vector{Complex}, signal2::Vector{Complex})
+function conv_lin(signal1::Array{Complex{Float64},1},
+                  signal2::Array{Complex{Float64},1})
     n = length(signal1) + length(signal2)
-    out = Vector{Complex}(n)
+    out = Array{Complex{Float64},1}(undef,n)
     sum = 0
 
-    for i = 0:n
-        for j = 0:i
-            if(j < length(signal1))
+    for i = 1:n-1
+        for j = 1:i-1
+            if j < length(signal1)
+                println(j, '\t', i, '\t', i-j)
                 sum += signal1[j] * signal2[i-j]
             end
         end
@@ -16,7 +18,8 @@ function conv(signal1::Vector{Complex}, signal2::Vector{Complex})
     return out
 end
 
-function conv_fft(signal1::Vector{Complex}, signal2::Vector{Complex})
+function conv_fft(signal1::Array{Complex{Float64},1},
+                  signal2::Array{Complex{Float64},1})
     return ifft(fft(signal1).*fft(signal2))
 end
 
